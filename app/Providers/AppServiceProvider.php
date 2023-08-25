cbef507561864f17e8e4d8079f72da52a0ae9a13
Andreas Müller <mail@devmount.de>
2023-08-25 18:03:21 +0200

    💚 use tabler icons

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Filament\Support\Colors\Color;
 use Filament\Support\Facades\FilamentColor;
+use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
@@ -31,6 +32,54 @@ class AppServiceProvider extends ServiceProvider
             'blue' => Color::Blue,
             'purple' => Color::Purple,
             'rose' => Color::Rose,
+        ]);
+
+        // Customize Filament icons
+        FilamentIcon::register([
+            // Panel Builder icon aliases
+            // 'panels::global-search.field' => 'tabler-', // Global search field
+            // 'panels::pages.dashboard.navigation-item' => 'tabler-', // Dashboard navigation item
+            // 'panels::pages.tenancy.register-tenant.open-tenant-button' => 'tabler-', // Button to open a tenant from the tenant registration page
+            // 'panels::sidebar.collapse-button' => 'tabler-', // Button to collapse the sidebar
+            // 'panels::sidebar.expand-button' => 'tabler-', // Button to expand the sidebar
+            // 'panels::sidebar.group.collapse-button' => 'tabler-', // Collapse button for a sidebar group
+            // 'panels::tenant-menu.toggle-button' => 'tabler-', // Button to toggle the tenant menu
+            // 'panels::theme-switcher.light-button' => 'tabler-', // Button to switch to the light theme from the theme switcher
+            // 'panels::theme-switcher.dark-button' => 'tabler-', // Button to switch to the dark theme from the theme switcher
+            // 'panels::theme-switcher.system-button' => 'tabler-', // Button to switch to the system theme from the theme switcher
+            // 'panels::topbar.close-sidebar-button' => 'tabler-', // Button to close the sidebar
+            // 'panels::topbar.open-sidebar-button' => 'tabler-', // Button to open the sidebar
+            // 'panels::topbar.open-database-notifications-button' => 'tabler-', // Button to open the database notifications modal
+            // 'panels::user-menu.profile-item' => 'tabler-', // Profile item in the user menu
+            // 'panels::user-menu.logout-button' => 'tabler-', // Button in the user menu to log out
+            // 'panels::widgets.account.logout-button' => 'tabler-', // Button in the account widget to log out
+            // 'panels::widgets.filament-info.open-documentation-button' => 'tabler-', // Button to open the documentation from the Filament info widget
+            // 'panels::widgets.filament-info.open-github-button' => 'tabler-', // Button to open GitHub from the Filament info widget
+
+            // Form Builder icon aliases
+            // 'forms:components.checkbox-list.search-field' => 'tabler-', // Search input in a checkbox list
+            // 'forms::components.wizard.completed-step' => 'tabler-', // Completed step in a wizard
+
+            // Table Builder icon aliases
+            // 'tables::columns.collapse-button' => 'tabler-columns-3',
+            // 'tables::filters.remove-all-button' => 'tabler-', // Button to remove all filters
+            // 'tables::grouping.collapse-button' => 'tabler-', // Button to collapse a group of records
+            'tables::header-cell.sort-asc-button' => 'tabler-chevron-up', // Sort button of a column sorted in ascending order
+            'tables::header-cell.sort-desc-button' => 'tabler-chevron-down', // Sort button of a column sorted in descending order
+            // 'tables::reorder.handle' => 'tabler-', // Handle to grab in order to reorder a record with drag and drop
+            'tables::search-field' => 'tabler-search', // Search input
+
+            // Notifications icon aliases
+            // 'notifications::database.modal.empty-state' => 'tabler-ban', // Empty state of the database notifications modal
+            // 'notifications::notification.close-button' => 'tabler-', // Button to close a notification
+
+            // UI components icon aliases
+            // 'badge.delete-button' => 'tabler-', // Button to delete a badge
+            'breadcrumbs.separator' => 'tabler-chevron-right', // Separator between breadcrumbs
+            'modal.close-button' => 'tabler-x', // Button to close a modal
+            'pagination.previous-button' => 'tabler-chevron-left', // Button to go to the previous page
+            'pagination.next-button' => 'tabler-chevron-right', // Button to go to the next page
+            // 'section.collapse-button' => 'tabler-', // Button to collapse a section
         ]);
     }
 }
