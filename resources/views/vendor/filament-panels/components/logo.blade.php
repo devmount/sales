23eb1437942758913edc4e4080abf86d48f2bb4b
Andreas Müller <mail@devmount.de>
2023-09-04 13:55:51 +0200

    ➕ add version and deployment

---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,14 @@
+@if (filled($brand = filament()->getBrandName()))
+    <div
+        {{
+            $attributes->class([
+                'fi-logo text-xl font-bold leading-5 tracking-tight text-gray-950 dark:text-white',
+            ])
+        }}
+    >
+        {{ $brand }}
+        <span class="text-gray-400 text-sm font-normal">
+            &nbsp;{{ config('app.version') }}
+        </span>
+    </div>
+@endif
