c7a9364362f9dced5b225f18c97b21ae3b932280
Andreas Müller <mail@devmount.de>
2023-09-17 20:31:54 +0200

    ➕ add two stats and one info widget to dashboard

---
+++ app/Filament/Widgets/StatsOverview.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Position;
+use Carbon\CarbonPeriod;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Filament\Support\Colors\Color;
+
+use function Filament\Support\format_money;
+use function Filament\Support\format_number;
+
+class StatsOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        [$revenue, $hours] = $this->getData();
+        $revenueStat = format_money($revenue[count($revenue)-1], 'eur');
+        $revenueDiff = $revenue[count($revenue)-1] - $revenue[count($revenue)-2];
+        $revenueIncrease = $revenueDiff >= 0;
+        $revenueDiffPercent = round(abs($revenueDiff)/$revenue[count($revenue)-2]*100);
+        $hoursStat = $hours[count($hours)-1];
+        $hoursDiff = $hours[count($hours)-1] - $hours[count($hours)-2];
+        $hoursIncrease = $hoursDiff >= 0;
+        $hoursDiffPercent = round(abs($hoursDiff)/$hours[count($hours)-2]*100);
+        return [
+            Stat::make('weeklyRevenue', $revenueStat)
+                ->label(__('weeklyRevenue'))
+                ->description($revenueDiffPercent . '% ' . ($revenueIncrease ? __('increase') : __('decrease')))
+                ->descriptionIcon($revenueIncrease ? 'tabler-trending-up': 'tabler-trending-down')
+                ->chart($revenue)
+                ->color($revenueIncrease ? Color::Blue : Color::Red),
+            Stat::make('weeklyWorkingHours', format_number($hoursStat))
+                ->label(__('weeklyWorkingHours'))
+                ->description($hoursDiffPercent . '% ' . ($hoursIncrease ? __('increase') : __('decrease')))
+                ->descriptionIcon($hoursIncrease ? 'tabler-trending-up': 'tabler-trending-down')
+                ->chart($hours)
+                ->color($hoursIncrease ? Color::Blue : Color::Red),
+            Stat::make('info', filament()->getBrandName() . ' v' . config('app.version'))
+                ->label('Filament ' . \Composer\InstalledVersions::getPrettyVersion('filament/filament'))
+                ->description('Laravel ' . \Composer\InstalledVersions::getPrettyVersion('laravel/framework')),
+        ];
+    }
+
+    protected function getData(): array
+    {
+        $positions = Position::where('started_at', '>', now()->subWeeks(7)->startOfWeek())->get();
+        $period = now()->subWeeks(7)->startOfWeek()->weeksUntil(now()->endOfWeek())->toArray();
+        $revenue = array_fill(0, count($period)-1, 0);
+        $hours = array_fill(0, count($period)-1, 0);
+        foreach ($period as $i => $date) {
+            if ($i == count($period)-1) break;
+            foreach ($positions as $obj) {
+                if (CarbonPeriod::create($date, $period[$i+1])->contains($obj->started_at)) {
+                    $revenue[$i] += $obj->net;
+                    $hours[$i] += $obj->duration;
+                }
+            }
+        }
+
+        return [$revenue, $hours];
+    }
+}
