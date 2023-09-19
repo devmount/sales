8f3405d31de72495c637054f9235bc952d0c3bec
Andreas Müller <mail@devmount.de>
2023-09-19 18:11:31 +0200

    🔨 allow expenses to be negative

---
+++ database/migrations/2023_09_19_000001_modify_expenses_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('expenses', function (Blueprint $table) {
+            $table->decimal('price', 8, 2, false)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('expenses', function (Blueprint $table) {
+            $table->decimal('price', 8, 2, true)->change();
+        });
+    }
+};
