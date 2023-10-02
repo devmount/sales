36ea1821c9c2ebe5a407c86aa5c1c9acbbc99a26
Andreas Müller <mail@devmount.de>
2023-10-02 17:03:29 +0200

    🔨 fix exported invoice language

--- resources/views/filament/resources/invoice-resource/pages/download-invoice.blade.php
+++ resources/views/filament/resources/invoice-resource/pages/download-invoice.blade.php
@@ -1,3 +1,7 @@
+@php
+    $lang = $this->record?->project?->client?->language ?? 'de';
+@endphp
+
 <x-filament-panels::page>
 {{ __('downloadStartsAutomatically' )}}
 
@@ -135,7 +139,7 @@ document.addEventListener('DOMContentLoaded', () => {
     doc.setFillColor(colors.main).rect(0, 9, 210, 30, 'F');
     doc.addImage(config.logo, 'JPEG', 12, 13, 22, 22);
     doc.setTextColor(colors.light).setFont('FiraSansExtraLight')
-        .setFontSize(26).text('{{ trans_choice("invoice", 1) }}'.toUpperCase(), 105, 27, { align: 'center' })
+        .setFontSize(26).text('{{ trans_choice("invoice", 1, [], $lang) }}'.toUpperCase(), 105, 27, { align: 'center' })
         .setFontSize(9)
             .text(config.email, 202, 25, { align: 'right' })
             .text(config.phone, 202, 19, { align: 'right' })
@@ -143,17 +147,17 @@ document.addEventListener('DOMContentLoaded', () => {
     // address header
     doc.setTextColor(colors.gray).setFont('FiraSansExtraLight').setFontSize(8)
         .text(config.address, 10, 50)
-        .setFontSize(9).text('{{ __("to") }}', 10, 62)
+        .setFontSize(9).text('{{ __("to", [], $lang) }}', 10, 62)
         .setTextColor(colors.main).setFontSize(15).text('{{ $this->record->project->client->name }}'.toUpperCase(), 10, 69)
         .setDrawColor(colors.line).setLineWidth(0.4).line(0, 73, 70, 73).line(140, 73, 210, 73)
         .setTextColor(colors.gray)
             .setFontSize(10)
                 .setLineHeightFactor(1.5).text('{{ str_replace("\n", "\\n", $this->record->project->client->address) }}', 10, 79)
-                .text('{{ __("invoiceNumber") }}', 144, 62.8)
-                .text('{{ __("invoiceDate") }}', 144, 68.8)
+                .text('{{ __("invoiceNumber", [], $lang) }}', 144, 62.8)
+                .text('{{ __("invoiceDate", [], $lang) }}', 144, 68.8)
             .setFont('FiraSansRegular').setTextColor(colors.main)
                 .text(invoiceNumber, 202, 62.8, { align: 'right' })
-                .text(hdate(today, '{{ $this->record->project->client->language }}'), 202, 68.8, { align: 'right' });
+                .text(hdate(today, '{{ $lang }}'), 202, 68.8, { align: 'right' });
     // invoice table content
     doc.setLineWidth(0.8)
         .setFillColor(colors.col3).rect(10, 105, 90, 56, 'F').setDrawColor(colors.col2).line(10, 133, 100, 133)
@@ -162,72 +166,72 @@ document.addEventListener('DOMContentLoaded', () => {
         .setFillColor(colors.accent).rect(162, 105, 40, 56, 'F').setDrawColor(colors.line2).line(162, 133, 202, 133)
         .setFontSize(13)
             .setFont('FiraSansExtraLight').setTextColor(colors.dark)
-                .text('{{ __("description") }}', 15, 118)
-                .text('{{ __("quantity") }}', 115, 118, { align: 'center' })
-                .text('{{ __("price") }}', 146, 118, { align: 'center' })
+                .text('{{ __("description", [], $lang) }}', 15, 118)
+                .text('{{ __("quantity", [], $lang) }}', 115, 118, { align: 'center' })
+                .text('{{ __("price", [], $lang) }}', 146, 118, { align: 'center' })
             .setFont('FiraSansRegular').setTextColor(colors.light)
-                .text('{{ __("total") }}', 182, 118, { align: 'center' })
+                .text('{{ __("total", [], $lang) }}', 182, 118, { align: 'center' })
         .setFontSize(8)
             .setFont('FiraSansExtraLight').setTextColor(colors.dark)
-                .text('{{ __("statementOfWork") }}', 15, 124)
-                .text(billedPerProject ? '{{ __("flatRate") }}' : '{{ __("inHours") }}', 115, 124, { align: 'center' })
-                .text(billedPerProject ? '{{ __("flatRate") }}' : '{{ __("perHour") }}', 146, 124, { align: 'center' })
+                .text('{{ __("statementOfWork", [], $lang) }}', 15, 124)
+                .text(billedPerProject ? '{{ __("flatRate", [], $lang) }}' : '{{ __("inHours", [], $lang) }}', 115, 124, { align: 'center' })
+                .text(billedPerProject ? '{{ __("flatRate", [], $lang) }}' : '{{ __("perHour", [], $lang) }}', 146, 124, { align: 'center' })
             .setTextColor(colors.light)
-                .text(billedPerProject ? '{{ __("sum") }}' : '{{ __("sumOfAllPositions") }}', 182, 124, { align: 'center' })
+                .text(billedPerProject ? '{{ __("sum", [], $lang) }}' : '{{ __("sumOfAllPositions", [], $lang) }}', 182, 124, { align: 'center' })
         .setTextColor(colors.dark).setFont('FiraSansRegular').setFontSize(9).text('{{ $this->record->title }}', 15, 141)
         .setFont('FiraSansExtraLight')
             .setFontSize(8)
                 .text('{{ str_replace("\n", "\\n", $this->record->description) }}', 15, 147)
             .setFontSize(16)
-                .text(nDigit(billedPerProject ? 1 : {{ $this->record->hours }}, 1, '{{ $this->record->project->client->language }}'), 115, 148, { align: 'center' })
-                .text(euro({{ $this->record->price }}, '{{ $this->record->project->client->language }}'), 146, 148, { align: 'center' })
+                .text(nDigit(billedPerProject ? 1 : {{ $this->record->hours }}, 1, '{{ $lang }}'), 115, 148, { align: 'center' })
+                .text(euro({{ $this->record->price }}, '{{ $lang }}'), 146, 148, { align: 'center' })
             .setTextColor(colors.light)
-                .text(euro({{ $this->record->net }}, '{{ $this->record->project->client->language }}'), 182, 148, { align: 'center' });
+                .text(euro({{ $this->record->net }}, '{{ $lang }}'), 182, 148, { align: 'center' });
     // invoice table total without discount
     if (!discount) {
         doc.setFillColor(colors.main).rect(0, 165, 210, 50, 'F').setDrawColor(colors.line3).setLineWidth(0.3).line(124, 196, 194, 196)
             .setTextColor(colors.text)
                 .setFont('FiraSansExtraLight').setFontSize(13)
-                    .text('{{ __("amountNet") }}', 160, 181, { align: 'right' })
-                    .text(percent('{{ $this->record->vat_rate }}', '{{ $this->record->project->client->language }}') + ' {{ __("vat") }}', 160, 190, { align: 'right' })
-                    .text(euro({{ $this->record->net }}, '{{ $this->record->project->client->language }}'), 194, 181, { align: 'right' })
-                    .text(euro({{ $this->record->vat }}, '{{ $this->record->project->client->language }}'), 194, 190, { align: 'right' })
+                    .text('{{ __("amountNet", [], $lang) }}', 160, 181, { align: 'right' })
+                    .text(percent('{{ $this->record->vat_rate }}', '{{ $lang }}') + ' {{ __("vat", [], $lang) }}', 160, 190, { align: 'right' })
+                    .text(euro({{ $this->record->net }}, '{{ $lang }}'), 194, 181, { align: 'right' })
+                    .text(euro({{ $this->record->vat }}, '{{ $lang }}'), 194, 190, { align: 'right' })
             .setTextColor(colors.light)
                 .setFont('FiraSansRegular').setFontSize(16)
-                    .text('{{ __("totalAmount") }}', 160, 205, { align: 'right' })
-                    .text(euro({{ $this->record->gross }}, '{{ $this->record->project->client->language }}'), 194, 205, { align: 'right' });
+                    .text('{{ __("totalAmount", [], $lang) }}', 160, 205, { align: 'right' })
+                    .text(euro({{ $this->record->gross }}, '{{ $lang }}'), 194, 205, { align: 'right' });
     }
     // invoice table total with discount
     else {
         doc.setFillColor(colors.main).rect(0, 165, 210, 50, 'F').setDrawColor(colors.line3).setLineWidth(0.3).line(124, 198, 194, 198)
             .setTextColor(colors.text)
                 .setFont('FiraSansExtraLight').setFontSize(13)
-                    .text('{{ __("amountNet") }}', 160, 177, { align: 'right' })
-                    .text('{{ __("credit") }}', 160, 185, { align: 'right' })
-                    .text(percent('{{ $this->record->vat_rate }}', '{{ $this->record->project->client->language }}') + ' {{ __("vat") }}', 160, 193, { align: 'right' })
-                    .text(euro({{ $this->record->net }}, '{{ $this->record->project->client->language }}'), 194, 177, { align: 'right' })
-                    .text('–' + euro(discount, '{{ $this->record->project->client->language }}'), 194, 185, { align: 'right' })
-                    .text(euro({{ $this->record->vat }}, '{{ $this->record->project->client->language }}'), 194, 193, { align: 'right' })
+                    .text('{{ __("amountNet", [], $lang) }}', 160, 177, { align: 'right' })
+                    .text('{{ __("credit", [], $lang) }}', 160, 185, { align: 'right' })
+                    .text(percent('{{ $this->record->vat_rate }}', '{{ $lang }}') + ' {{ __("vat", [], $lang) }}', 160, 193, { align: 'right' })
+                    .text(euro({{ $this->record->net }}, '{{ $lang }}'), 194, 177, { align: 'right' })
+                    .text('–' + euro(discount, '{{ $lang }}'), 194, 185, { align: 'right' })
+                    .text(euro({{ $this->record->vat }}, '{{ $lang }}'), 194, 193, { align: 'right' })
             .setTextColor(colors.light)
                 .setFont('FiraSansRegular').setFontSize(16)
-                    .text('{{ __("totalAmount") }}', 160, 207, { align: 'right' })
-                    .text(euro({{ $this->record->gross }}, '{{ $this->record->project->client->language }}'), 194, 207, { align: 'right' });
+                    .text('{{ __("totalAmount", [], $lang) }}', 160, 207, { align: 'right' })
+                    .text(euro({{ $this->record->gross }}, '{{ $lang }}'), 194, 207, { align: 'right' });
     }
     // terms
     doc.setFontSize(10).setFont('FiraSansExtraLight').setTextColor(colors.dark)
         .text(markerReplace(
-            '{{ __("invoice.explanation") }}',
-            [euro({{ $this->record->gross }}, '{{ $this->record->project->client->language }}'), invoiceNumber]
+            '{{ __("invoice.explanation", [], $lang) }}',
+            [euro({{ $this->record->gross }}, '{{ $lang }}'), invoiceNumber]
         ), 10, 225, { maxWidth: 180 })
-        .text(['{{ __("withKindRegards") }}', config.name], 10, 244);
+        .text(['{{ __("withKindRegards", [], $lang) }}', config.name], 10, 244);
     // footer
     doc.setDrawColor(colors.line).setLineWidth(0.4).line(10, 272, 202, 272)
         .addImage(config.signature, 'PNG', 13, 255, 24, 18)
         .setLineHeightFactor(1.3).setFontSize(9).setTextColor(colors.gray)
-            .text('{{ __("page") }} ' + page + '/' + totalPageCount, 202, 290, { align: 'right' })
-            .text('Berlin, ' + hdate(today, '{{ $this->record->project->client->language }}'), 10, 277)
-            .text(['{{ __("iban") }}', '{{ __("bic") }}', '{{ __("bank") }}', '{{ __("holder") }}'], 90, 277, { align: 'right' })
-            .text(['{{ __("vatId") }}', '{{ __("taxOffice") }}'], 170, 277, { align: 'right' })
+            .text('{{ __("page", [], $lang) }} ' + page + '/' + totalPageCount, 202, 290, { align: 'right' })
+            .text('Berlin, ' + hdate(today, '{{ $lang }}'), 10, 277)
+            .text(['{{ __("iban", [], $lang) }}', '{{ __("bic", [], $lang) }}', '{{ __("bank", [], $lang) }}', '{{ __("holder", [], $lang) }}'], 90, 277, { align: 'right' })
+            .text(['{{ __("vatId", [], $lang) }}', '{{ __("taxOffice", [], $lang) }}'], 170, 277, { align: 'right' })
         .setFont('FiraSansRegular')
             .text([config.iban, config.bic, config.bank, config.accountHolder], 92, 277)
             .text([config.vatId, config.taxOffice], 172, 277);
@@ -240,7 +244,7 @@ document.addEventListener('DOMContentLoaded', () => {
         doc.setFillColor(colors.main).rect(0, 9, 210, 30, 'F');
         doc.addImage(config.logo, 'JPEG', 12, 13, 22, 22);
         doc.setTextColor(colors.light).setFont('FiraSansExtraLight')
-            .setFontSize(26).text('{{ __("deliverables") }}'.toUpperCase(), 105, 27, { align: 'center' })
+            .setFontSize(26).text('{{ __("deliverables", [], $lang) }}'.toUpperCase(), 105, 27, { align: 'center' })
             .setFontSize(9)
                 .text(config.email, 202, 25, { align: 'right' })
                 .text(config.phone, 202, 19, { align: 'right' })
@@ -261,29 +265,29 @@ document.addEventListener('DOMContentLoaded', () => {
                 .setDrawColor(billedPerProject ? colors.col2 : colors.line2).line(176, 78, 202, 78)
             .setFontSize(13)
                 .setFont('FiraSansExtraLight').setTextColor(colors.dark)
-                    .text('{{ trans_choice("position", 1) }}', 15, 63)
-                    .text(billedPerProject ? '' : '{{ __("quantity") }}', 136, 63, { align: 'center' })
-                    .text(billedPerProject ? '' : '{{ __("price") }}', 162, 63, { align: 'center' })
+                    .text('{{ trans_choice("position", 1, [], $lang) }}', 15, 63)
+                    .text(billedPerProject ? '' : '{{ __("quantity", [], $lang) }}', 136, 63, { align: 'center' })
+                    .text(billedPerProject ? '' : '{{ __("price", [], $lang) }}', 162, 63, { align: 'center' })
                 .setFont('FiraSansRegular').setTextColor(colors.light)
-                    .text(billedPerProject ? '' : '{{ __("total") }}', 189, 63, { align: 'center' })
+                    .text(billedPerProject ? '' : '{{ __("total", [], $lang) }}', 189, 63, { align: 'center' })
             .setFontSize(8)
                 .setFont('FiraSansExtraLight').setTextColor(colors.dark)
-                    .text(undated ? '{{ __("description") }}' : '{{ __("dateAndDescription") }}', 15, 69)
-                    .text(billedPerProject ? '' : '{{ __("inHours") }}', 136, 69, { align: 'center' })
-                    .text(billedPerProject ? '' : '{{ __("perHour") }}', 162, 69, { align: 'center' })
+                    .text(undated ? '{{ __("description", [], $lang) }}' : '{{ __("dateAndDescription", [], $lang) }}', 15, 69)
+                    .text(billedPerProject ? '' : '{{ __("inHours", [], $lang) }}', 136, 69, { align: 'center' })
+                    .text(billedPerProject ? '' : '{{ __("perHour", [], $lang) }}', 162, 69, { align: 'center' })
                 .setTextColor(colors.light)
-                    .text(billedPerProject ? '' : '{{ __("price") }}', 189, 69, { align: 'center' });
+                    .text(billedPerProject ? '' : '{{ __("price", [], $lang) }}', 189, 69, { align: 'center' });
         // draw positions
         let linesProcessed = 0;
         positions.forEach((p, i) => {
-            const posdate = hdate(new Date(p.started_at), '{{ $this->record->project->client->language }}');
+            const posdate = hdate(new Date(p.started_at), '{{ $lang }}');
             const poshours = positionDuration(p);
             const lineCount = p.description.split('\n').length + 2;
             doc.setTextColor(colors.dark)
                 .setFont('FiraSansRegular')
                     .setFontSize(9)
                         .text(
-                            undated ? (i+1).toString() + '. {{ trans_choice("position", 1) }}' : posdate,
+                            undated ? (i+1).toString() + '. {{ trans_choice("position", 1, [], $lang) }}' : posdate,
                             15,
                             (84+positionRowHeight*linesProcessed)
                         )
@@ -292,17 +296,17 @@ document.addEventListener('DOMContentLoaded', () => {
                         .text(p.description, 15, (88+positionRowHeight*linesProcessed))
                     .setFontSize(11)
                         .text(
-                            billedPerProject ? '' : nDigit(poshours, 1, '{{ $this->record->project->client->language }}'),
+                            billedPerProject ? '' : nDigit(poshours, 1, '{{ $lang }}'),
                             136,
                             (87+positionRowHeight*linesProcessed), { align: 'center' }
                         ).text(
-                            billedPerProject ? '' : euro({{ $this->record->price }}, '{{ $this->record->project->client->language }}'),
+                            billedPerProject ? '' : euro({{ $this->record->price }}, '{{ $lang }}'),
                             162,
                             (87+positionRowHeight*linesProcessed), { align: 'center' }
                         )
                     .setTextColor(colors.light)
                         .text(
-                            billedPerProject ? '' : euro(poshours*{{ $this->record->price }}, '{{ $this->record->project->client->language }}'),
+                            billedPerProject ? '' : euro(poshours*{{ $this->record->price }}, '{{ $lang }}'),
                             189,
                             (87+positionRowHeight*linesProcessed), { align: 'center' }
                         );
@@ -312,10 +316,10 @@ document.addEventListener('DOMContentLoaded', () => {
         doc.setDrawColor(colors.line).setLineWidth(0.4).line(10, 272, 202, 272)
             .addImage(config.signature, 'PNG', 13, 255, 24, 18)
             .setLineHeightFactor(1.3).setFontSize(9).setTextColor(colors.gray)
-                .text('{{ __("page") }} ' + page + '/' + totalPageCount, 202, 290, { align: 'right' })
-                .text('Berlin, ' + hdate(today, '{{ $this->record->project->client->language }}'), 10, 277)
-                .text(['{{ __("iban") }}', '{{ __("bic") }}', '{{ __("bank") }}', '{{ __("holder") }}'], 90, 277, { align: 'right' })
-                .text(['{{ __("vatId") }}', '{{ __("taxOffice") }}'], 170, 277, { align: 'right' })
+                .text('{{ __("page", [], $lang) }} ' + page + '/' + totalPageCount, 202, 290, { align: 'right' })
+                .text('Berlin, ' + hdate(today, '{{ $lang }}'), 10, 277)
+                .text(['{{ __("iban", [], $lang) }}', '{{ __("bic", [], $lang) }}', '{{ __("bank", [], $lang) }}', '{{ __("holder", [], $lang) }}'], 90, 277, { align: 'right' })
+                .text(['{{ __("vatId", [], $lang) }}', '{{ __("taxOffice", [], $lang) }}'], 170, 277, { align: 'right' })
             .setFont('FiraSansRegular')
                 .text([config.iban, config.bic, config.bank, config.accountHolder], 92, 277)
                 .text([config.vatId, config.taxOffice], 172, 277);
@@ -323,7 +327,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     // serve document
     doc.save(
-        `${invoiceNumber}_{{ trans_choice("invoice", 1, [], $this->record->project->client->language) }}_${config.company}.pdf`.toLowerCase(),
+        `${invoiceNumber}_{{ trans_choice("invoice", 1, [], $lang) }}_${config.company}.pdf`.toLowerCase(),
         { returnPromise: true }
     ).then(() => {
         setTimeout(() => { window.close() }, 100);
