da335a856fafce6e195d162a9a8d45c9c3cc35cd
Andreas Müller <mail@devmount.de>
2023-11-21 11:55:35 +0100

    🔨 add missing setting

--- database/migrations/2023_09_01_181022_create_settings_table.php
+++ database/migrations/2023_09_01_181022_create_settings_table.php
@@ -20,20 +20,21 @@ return new class extends Migration
             $table->integer('weight')->default(0);
             $table->timestamps();
         });
-        Setting::create(['field' => 'name',          'type' => 'text',     'weight' =>  10]);
-        Setting::create(['field' => 'company',       'type' => 'text',     'weight' =>  20]);
-        Setting::create(['field' => 'address',       'type' => 'textarea', 'weight' =>  30]);
-        Setting::create(['field' => 'email',         'type' => 'email',    'weight' =>  40]);
-        Setting::create(['field' => 'phone',         'type' => 'tel',      'weight' =>  50]);
-        Setting::create(['field' => 'website',       'type' => 'url',      'weight' =>  60]);
-        Setting::create(['field' => 'iban',          'type' => 'text',     'weight' =>  70]);
-        Setting::create(['field' => 'bic',           'type' => 'text',     'weight' =>  80]);
-        Setting::create(['field' => 'bank',          'type' => 'text',     'weight' =>  90]);
         Setting::create(['field' => 'accountHolder', 'type' => 'text',     'weight' => 100]);
+        Setting::create(['field' => 'address',       'type' => 'textarea', 'weight' =>  30]);
+        Setting::create(['field' => 'bank',          'type' => 'text',     'weight' =>  90]);
+        Setting::create(['field' => 'bic',           'type' => 'text',     'weight' =>  80]);
+        Setting::create(['field' => 'company',       'type' => 'text',     'weight' =>  20]);
+        Setting::create(['field' => 'email',         'type' => 'email',    'weight' =>  40]);
+        Setting::create(['field' => 'iban',          'type' => 'text',     'weight' =>  70]);
+        Setting::create(['field' => 'logo',          'type' => 'textarea', 'weight' => 130]);
+        Setting::create(['field' => 'name',          'type' => 'text',     'weight' =>  10]);
+        Setting::create(['field' => 'phone',         'type' => 'tel',      'weight' =>  50]);
+        Setting::create(['field' => 'signature',     'type' => 'textarea', 'weight' => 140]);
         Setting::create(['field' => 'taxOffice',     'type' => 'text',     'weight' => 110]);
         Setting::create(['field' => 'vatId',         'type' => 'text',     'weight' => 120]);
-        Setting::create(['field' => 'logo',          'type' => 'textarea', 'weight' => 130]);
-        Setting::create(['field' => 'signature',     'type' => 'textarea', 'weight' => 140]);
+        Setting::create(['field' => 'vatRate',       'type' => 'number',   'weight' => 130]);
+        Setting::create(['field' => 'website',       'type' => 'url',      'weight' =>  60]);
     }
 
     /**
