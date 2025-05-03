a39e0d1fc835320f9e0aa7b67b18ab1b247a8a19
Andreas Müller <mail@devmount.de>
2025-05-03 23:31:57 +0200

    ➕ Add offtime model

---
+++ app/Models/Offtime.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\OfftimeCategory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Offtime extends Model
+{
+    use HasFactory;
+
+    protected function casts(): array
+    {
+        return [
+            'start' => 'date',
+            'end' => 'date',
+            'category' => OfftimeCategory::class,
+        ];
+    }
+
+    /**
+     * Year the time off is assigned to
+     */
+    public function getYearAttribute(): int
+    {
+        return intval($this->start->format('Y'));
+    }
+
+    /**
+     * Year the time off is assigned to
+     */
+    public function getDaysCountAttribute(): int
+    {
+        return $this->end
+            ? $this->start->diffInDays($this->end)
+            : 1;
+    }
+
+}
