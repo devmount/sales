8172db236d17e66637ef05b006f060cecd4761bb
Andreas Müller <mail@devmount.de>
2025-05-31 21:55:25 +0200

    🔨 Reuse form fields for gifts

--- app/Filament/Resources/GiftResource.php
+++ app/Filament/Resources/GiftResource.php
@@ -9,6 +9,7 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Support\Enums\Alignment;
 use Filament\Support\Enums\FontFamily;
+use Filament\Support\Enums\MaxWidth;
 use Filament\Tables\Actions;
 use Filament\Tables\Columns\Summarizers\Sum;
 use Filament\Tables\Columns\TextColumn;
@@ -22,41 +23,7 @@ class GiftResource extends Resource
 
     public static function form(Form $form): Form
     {
-        return $form
-            ->schema([
-                Components\Section::make()
-                    ->columns(12)
-                    ->schema([
-                        Components\DatePicker::make('received_at')
-                            ->label(__('receivedAt'))
-                            ->columnSpan(3)
-                            ->weekStartsOnMonday()
-                            ->suffixIcon('tabler-calendar-heart')
-                            ->required(),
-                        Components\TextInput::make('amount')
-                            ->label(__('amount'))
-                            ->columnSpan(3)
-                            ->numeric()
-                            ->step(0.01)
-                            ->minValue(0.01)
-                            ->suffixIcon('tabler-currency-euro')
-                            ->required(),
-                        Components\TextInput::make('subject')
-                            ->label(__('subject'))
-                            ->columnSpan(6)
-                            ->suffixIcon('tabler-sticker')
-                            ->required(),
-                        Components\TextInput::make('name')
-                            ->label(__('name'))
-                            ->columnSpan(6)
-                            ->suffixIcon('tabler-id'),
-                        Components\TextInput::make('email')
-                            ->label(__('email'))
-                            ->columnSpan(6)
-                            ->email()
-                            ->suffixIcon('tabler-mail'),
-                    ])
-            ]);
+        return $form->schema(self::formFields())->columns(2);
     }
 
     public static function table(Table $table): Table
@@ -82,8 +49,7 @@ class GiftResource extends Resource
                     ->fontFamily(FontFamily::Mono)
                     ->alignment(Alignment::End)
                     ->sortable()
-                    ->summarize(Sum::make()
-                    ->money('eur')),
+                    ->summarize(Sum::make()->money('eur')),
                 TextColumn::make('created_at')
                     ->label(__('createdAt'))
                     ->datetime('j. F Y, H:i:s')
@@ -100,9 +66,11 @@ class GiftResource extends Resource
             ])
             ->actions(
                 Actions\ActionGroup::make([
-                    Actions\EditAction::make()->icon('tabler-edit'),
-                    Actions\ReplicateAction::make()->icon('tabler-copy'),
-                    Actions\DeleteAction::make()->icon('tabler-trash'),
+                    Actions\EditAction::make()->icon('tabler-edit')->slideOver()->modalWidth(MaxWidth::Large),
+                    Actions\ReplicateAction::make()->icon('tabler-copy')->form([
+                        Components\Grid::make()->columns(2)->schema(self::formFields())
+                    ])->slideOver()->modalWidth(MaxWidth::Large),
+                    Actions\DeleteAction::make()->icon('tabler-trash')->requiresConfirmation(),
                 ])
                 ->icon('tabler-dots-vertical')
             )
@@ -113,7 +81,7 @@ class GiftResource extends Resource
                 ->icon('tabler-dots-vertical'),
             ])
             ->emptyStateActions([
-                Actions\CreateAction::make()->icon('tabler-plus'),
+                Actions\CreateAction::make()->icon('tabler-plus')->slideOver()->modalWidth(MaxWidth::Large),
             ])
             ->emptyStateIcon('tabler-ban')
             ->defaultSort('received_at', 'desc')
@@ -124,8 +92,6 @@ class GiftResource extends Resource
     {
         return [
             'index' => Pages\ListGifts::route('/'),
-            'create' => Pages\CreateGift::route('/create'),
-            'edit' => Pages\EditGift::route('/{record}/edit'),
         ];
     }
 
@@ -147,6 +113,36 @@ class GiftResource extends Resource
     public static function getPluralModelLabel(): string
     {
         return trans_choice('gift', 2);
+    }
+
+    public static function formFields(): array
+    {
+        return [
+            Components\DatePicker::make('received_at')
+                ->label(__('receivedAt'))
+                ->weekStartsOnMonday()
+                ->suffixIcon('tabler-calendar-heart')
+                ->required(),
+            Components\TextInput::make('amount')
+                ->label(__('amount'))
+                ->numeric()
+                ->step(0.01)
+                ->minValue(0.01)
+                ->suffixIcon('tabler-currency-euro')
+                ->required(),
+            Components\TextInput::make('subject')
+                ->label(__('subject'))
+                ->columnSpanFull()
+                ->suffixIcon('tabler-sticker')
+                ->required(),
+            Components\TextInput::make('name')
+                ->label(__('name'))
+                ->suffixIcon('tabler-id'),
+            Components\TextInput::make('email')
+                ->label(__('email'))
+                ->email()
+                ->suffixIcon('tabler-mail'),
+        ];
     }
 
 }
--- app/Filament/Resources/GiftResource/Pages/ListGifts.php
+++ app/Filament/Resources/GiftResource/Pages/ListGifts.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\GiftResource\Pages;
 use App\Filament\Resources\GiftResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Support\Enums\MaxWidth;
 
 class ListGifts extends ListRecords
 {
@@ -13,7 +14,10 @@ class ListGifts extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make()->icon('tabler-plus'),
+            Actions\CreateAction::make()
+                ->icon('tabler-plus')
+                ->slideOver()
+                ->modalWidth(MaxWidth::Large),
         ];
     }
 }
