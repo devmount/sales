6deb881a4c9042c747518f809e372281c4446167
Andreas Müller <mail@devmount.de>
2026-07-08 07:45:10 +0200

    🔨 Improve user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,19 +20,19 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'email_verified_at' => null,
+            'password' => 'password',
             'remember_token' => Str::random(10),
         ];
     }
 
     /**
-     * Indicate that the model's email address should be unverified.
+     * Indicate that the model's email address should be verified.
      */
-    public function unverified(): static
+    public function verified(): static
     {
         return $this->state(fn () => [
-            'email_verified_at' => null,
+            'email_verified_at' => now(),
         ]);
     }
 }
