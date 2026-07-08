04ac2bc3060e58910ba1798e8e9422100b7ab33d
Andreas Müller <mail@devmount.de>
2026-07-08 09:54:49 +0200

    ➕️ Init testing with Pest

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -20,19 +21,19 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => null,
-            'password' => 'password',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
 
     /**
-     * Indicate that the model's email address should be verified.
+     * Indicate that the model's email address should be unverified.
      */
-    public function verified(): static
+    public function unverified(): static
     {
         return $this->state(fn () => [
-            'email_verified_at' => now(),
+            'email_verified_at' => null,
         ]);
     }
 }
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,19 +1,10 @@
 <?php
 
-namespace Tests\Feature;
+use App\Models\User;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
+it('returns a successful response', function () {
+    $user = User::factory()->create();
+    $response = $this->actingAs($user)->get('/');
 
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-}
+    $response->assertStatus(200);
+});
