33bc21d60a174719fb2dd64e6b8956f50b3e35ba
Andreas Müller <mail@devmount.de>
2026-09-11 00:10:21 +0200

    🔨 Fix overlapping borders of time buckets

--- tests/Feature/MonthlyIncomeChartTest.php
+++ tests/Feature/MonthlyIncomeChartTest.php
@@ -68,4 +68,33 @@ class MonthlyIncomeChartTest extends TestCase
         $this->assertNotFalse($yearIndex);
         $this->assertSame(99.17, $data['datasets'][0]['data'][$yearIndex]);
     }
+
+    #[Test]
+    public function it_does_not_double_count_an_invoice_paid_exactly_on_a_year_boundary(): void
+    {
+        $anchorYear = now()->year - 4;
+        $boundaryYear = now()->year - 2;
+
+        // establishes the chart's period start well before the boundary under test
+        Invoice::factory()
+            ->for(Project::factory())
+            ->create(['paid_at' => "$anchorYear-01-15", 'transitory' => false, 'pricing_unit' => PricingUnit::Project, 'price' => 12, 'discount' => null, 'taxable' => false]);
+
+        // paid exactly on the year boundary: must count only for $boundaryYear, not also for $boundaryYear - 1
+        Invoice::factory()
+            ->for(Project::factory())
+            ->create(['paid_at' => "$boundaryYear-01-01", 'transitory' => false, 'pricing_unit' => PricingUnit::Project, 'price' => 1200, 'discount' => null, 'taxable' => false]);
+
+        $widget = new MonthlyIncomeChart();
+        $widget->filter = 'net';
+        $data = (new ReflectionMethod($widget, 'getData'))->invoke($widget);
+
+        $previousYearIndex = array_search((string) ($boundaryYear - 1), $data['labels'], true);
+        $boundaryYearIndex = array_search((string) $boundaryYear, $data['labels'], true);
+
+        $this->assertNotFalse($previousYearIndex);
+        $this->assertNotFalse($boundaryYearIndex);
+        $this->assertSame(0.0, $data['datasets'][0]['data'][$previousYearIndex]);
+        $this->assertSame(100.0, $data['datasets'][0]['data'][$boundaryYearIndex]);
+    }
 }
